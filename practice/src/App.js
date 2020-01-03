ee515ea876f733fb5b8b792cce041e6f294bd09a
Justin Trombley <jtrombley96@gmail.com>
2020-01-02 19:05:07 -0500

    Class Component Map from State

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+	constructor () {
+		super();
 
+		this.state = {
+			killers: [
+        {
+          name: 'Leatherface',
+          id: 1,
+        },
+        {
+          name: 'Jason',
+          id: 2,
+        },
+        {
+          name: 'Agness',
+          id: 3,
+        },
+        {
+          name: 'Freddy',
+          id: 4,
+        }
+      ],
+		};
+	}
+	render () {
+		return (
+			<div className='App'>
+        {
+          this.state.killers.map(killer => <h1 key = {killer.id}>{killer.name}</h1>)
+        }
+			</div>
+		);
+	}
+}
 export default App;
